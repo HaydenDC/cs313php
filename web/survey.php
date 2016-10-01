e5d6c18ab90b3a9a527f6976ef8da9e3e8688f9a
Hayden <car12039@byui.edu>
2016-10-01 00:35:29 -0600

    survey.php

--- web/survey.php
+++ web/survey.php
@@ -1,5 +1,15 @@
 <html>
 <body>
- Hello <?php echo htmlspecialchars($_POST["name"]); ?>
+ Hello <?php echo htmlspecialchars($_POST["name"]); ?>!!
+ Your favorite sport is: <?php echo htmlspecialchars($_POST["sport"]); ?>.
+ Your favorite team is: <?php echo htmlspecialchars($_POST["team"]); ?>.
+ <?php
+  if($_POST["beenToGame"]="Yes"){
+ 	echo "You have been to a game";
+ } 
+ else{
+ 	echo "You really should go to a game..";
+ }
+ ?>
 </body>
 </html>
