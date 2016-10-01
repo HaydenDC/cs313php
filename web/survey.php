ef1afc38bcb8c5b56e9bd5164b47d06876c3cdc4
Hayden <car12039@byui.edu>
2016-10-01 01:18:02 -0600

    survey.php

--- web/survey.php
+++ web/survey.php
@@ -1,16 +1,16 @@
 <html>
 <body>
-<!--Users name-->
+
  Hello <?php echo htmlspecialchars($_POST["name"]); ?>!!
 
- <!--Favorite Sport-->
+ 
  Your favorite sport is: <?php echo htmlspecialchars($_POST["sport"]); ?>.
 
- <!--Favorite Team-->
- Your favorite team is: <?php echo htmlspecialchars($_POST["team"]); ?>.
+ 
+ Your favorite team is: <?php echo htmlspecialchars($_POST["team"]); ?>.	
 
  
- <!--Have they been to a game?-->
+ 
  <?php
   if($_POST["beenToGame"]=="Yes"){
  	echo "You have been to a game";
@@ -21,7 +21,7 @@
  ?><br>
 
  
-<!-- Have they played the sport?-->
+
  <?php
   if($_POST["havePlayed"])=="Yes"){
 	echo "You have played the sport";
@@ -32,7 +32,7 @@ else{
 ?>
 
 
-<!--How good are they? using a switch statement-->
+
 Based on how good you are, we think
 </body>
 </html>
