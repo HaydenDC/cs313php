78d966535abb2b8fd0fc7b87d1a00b828b1e54bb
Hayden <car12039@byui.edu>
2016-10-01 11:18:47 -0600

    survey.css

--- web/survey.php
+++ web/survey.php
@@ -17,7 +17,7 @@ $myFile = fopen("newFile.txt","w") or die("Unable to open/write file:(");
 $name = " mystery person";
 
 if(!$_POST["name"]){
-	fwrite($myFile, "Hello" . $name . "!!");
+	file_put_contents($myFile, "Hello" . $name . "!!");
 }
 else{
 $name = htmlspecialchars($_POST["name"]);
