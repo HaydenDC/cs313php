a3194daa5fb2068ef0c4580fba3c728b4b889844
Hayden <car12039@byui.edu>
2016-10-01 01:29:16 -0600

    survey.php

--- web/survey.php
+++ web/survey.php
@@ -32,7 +32,52 @@ else{
 }
 ?>
 
+Based on how good you are, we think
+<?php
 
+switch($_POST["howGood"]){
+	case "1":
+	echo "YOU SHOULD GET OUT THERE AND PLAY MORE!!";
+	break;
+
+	case "2":
+	echo "YOU SHOULD GO PLAY SOME MORE!";
+	break;
+
+	case "3":
+	echo "thats ok. It's not meant for everyone.";
+	break;
+
+	case "4":
+	echo "good for you.";
+	break;
+
+	case "5":
+	echo "you are mediocre";
+	break;
+
+	case "6":
+	echo "you are not too bad, but could still use some work.";
+	break;
+
+	case "7":
+	echo "7? Are you sure?";
+	break;
+
+	case "8":
+	echo "thats great! You must be really good!";
+	break;
+
+	case "9":
+	echo "that you should be on our team! We need good players!";
+	break;
+
+	case "10":
+	echo "since you aren't a pro, you must be full of yourself. Sorry.";
+	break;
+	
+}
+?>
 
 
 </body>
