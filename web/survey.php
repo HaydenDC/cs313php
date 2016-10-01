c680c0ca7225e1c74ee904dd336c90120a35948e
Hayden <car12039@byui.edu>
2016-10-01 02:05:33 -0600

    survey.php

--- web/survey.php
+++ web/survey.php
@@ -2,84 +2,102 @@
 <body>
 
 <h1>This is the results page</h1>
- Hello <?php echo htmlspecialchars($_POST["name"]); ?>!!<br>
 
- 
- Your favorite sport is: <?php echo htmlspecialchars($_POST["sport"]); ?>.<br>
+<!--Creat the file-->
+<?php
+//Make the file
+$myFile = fopen("newFile.txt","W") or die("Unable to open/write file:(");
 
- 
- Your favorite team is: <?php echo htmlspecialchars($_POST["team"]); ?>.<br>
+//Users name
+$name = htmlspecialchars($_POST["name"]);
+fwrite($myFile, $name);
 
- 
- 
- <?php
-  if($_POST["beenToGame"]=="Yes"){
- 	echo "We are glad you have been to a game";
+//Users favorite sport
+$favSport = htmlspecialchars($_POST["sport"]);
+fwrite($myFile, $sport);
+
+//Users favorite team
+$favTeam = htmlspecialchars($_POST["team"]);
+fwrite($myFile, $team);
+
+//Has the user been to a game?
+$yGame = "You dont know if you have been to a game?";
+if($_POST["beenToGame"]=="Yes"){
+ 	$yGame = "We are glad you have been to a game";
  } 
  else{
- 	echo "You really should go to a game..";
+ 	$yGame = "You really should go to a game..";
  }
- ?><br>
+ fwrite($myFile, $yGame);
 
- 
-
- <?php
- 
-  if($_POST["havePlayed"]=="Yes"){
-	echo "We love that you have played the sport";
+ //Does the user play the sport?
+ $yPlay = "You dont know if you have played the sport?"
+if($_POST["havePlayed"]=="Yes"){
+	$yPlay = "We love that you have played the sport";
 }
 else{
-	echo "No? Then how do you know it's your favorite?"; 
-}
-?><br>
+	$yPlay = "No? Then how do you know it's your favorite?"; 
+} 
+fwrite($myFile, $yPlay);
 
-Based on how good you are, we think
-<?php
-
+//How good is the user?
+$hGood = "you should actually tell us how good you think you are."
 switch($_POST["howGood"]){
 	case "1":
-	echo "YOU SHOULD GET OUT THERE AND PLAY MORE!!";
+	$hGood = "YOU SHOULD GET OUT THERE AND PLAY MORE!!";
 	break;
 
 	case "2":
-	echo "YOU SHOULD GO PLAY SOME MORE!";
+	$hGood = "YOU SHOULD GO PLAY SOME MORE!";
 	break;
 
 	case "3":
-	echo "thats ok. It's not meant for everyone.";
+	$hGood = "thats ok. It's not meant for everyone.";
 	break;
 
 	case "4":
-	echo "good for you.";
+	$hGood = "good for you.";
 	break;
 
 	case "5":
-	echo "you are mediocre";
+	$hGood = "you are mediocre";
 	break;
 
 	case "6":
-	echo "you are not too bad, but could still use some work.";
+	$hGood = "you are not too bad, but could still use some work.";
 	break;
 
 	case "7":
-	echo "7? Are you sure?";
+	$hGood = "7? Are you sure?";
 	break;
 
 	case "8":
-	echo "thats great! You must be really good!";
+	$hGood = "thats great! You must be really good!";
 	break;
 
 	case "9":
-	echo "that you should be on our team! We need good players!";
+	$hGood = "that you should be on our team! We need good players!";
 	break;
 
 	case "10":
-	echo "since you aren't a pro, you must be full of yourself. Sorry.";
+	$hGood = "since you aren't a pro, you must be full of yourself. Sorry.";
 	break;
-	
 }
-?><br>
+fwrite($myFile, $hGood);
+fclose($myFile);
 
+
+?>
+
+<!--Open the file-->
+<?php
+$myFile = fopen("newFile.txt", "r") or die ("Couldn't open the file dude");
+echo fread($myFile, filesize("newFile.txt"));
+fclose($myFile);
+?>
+
+<h1>Did the file open</h1>
+ 
 
 </body>
 </html>
