c6cfda1f9b1da1f1933a28772210f7e5d21fd5e6
Hayden <car12039@byui.edu>
2016-10-02 14:35:38 -0600

    dragonsHome.html

--- web/dragonsJS.js
+++ web/dragonsJS.js
@@ -9,3 +9,15 @@ function loadDoc() {
   xhttp.open("GET", "https://morning-basin-25235.herokuapp.com/dragonsHP.html", true);
   xhttp.send();
 }
+
+//Load Login
+function loadLogIn() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+     document.getElementById("content").innerHTML = xhttp.responseText;
+    }
+  };
+  xhttp.open("GET", "https://soundersfc.com", true);
+  xhttp.send();
+}
