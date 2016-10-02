bafa2669191cd2340345af3066bef13013d9a00b
Hayden <car12039@byui.edu>
2016-10-02 15:28:39 -0600

    dragonsHome.html

--- web/dragonsJS.js
+++ web/dragonsJS.js
@@ -21,3 +21,51 @@ function loadLogIn() {
   xhttp.open("GET", "https://morning-basin-25235.herokuapp.com/dragonsLogIn.html", true);
   xhttp.send();
 }
+
+//Load Roster
+function loadRoster() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+     document.getElementById("content").innerHTML = xhttp.responseText;
+    }
+  };
+  xhttp.open("GET", "https://morning-basin-25235.herokuapp.com/dragonsRoster.html", true);
+  xhttp.send();
+}
+
+//Load Schedule
+function loadSchedule() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+     document.getElementById("content").innerHTML = xhttp.responseText;
+    }
+  };
+  xhttp.open("GET", "https://morning-basin-25235.herokuapp.com/dragonsSchedule.html", true);
+  xhttp.send();
+}
+
+//Load Alumni
+function loadAlumni() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+     document.getElementById("content").innerHTML = xhttp.responseText;
+    }
+  };
+  xhttp.open("GET", "https://morning-basin-25235.herokuapp.com/dragonsAlumni.html", true);
+  xhttp.send();
+}
+
+//Load Information
+function loadInfo() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+     document.getElementById("content").innerHTML = xhttp.responseText;
+    }
+  };
+  xhttp.open("GET", "https://morning-basin-25235.herokuapp.com/dragonsInfo.html", true);
+  xhttp.send();
+}
