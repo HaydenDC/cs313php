e90679c1f6ed12ba62a9c094d3fee49dab144559
Hayden <car12039@byui.edu>
2016-10-15 13:33:04 -0600

    Created dragonsAlumni.php

---
+++ web/dragonsAlumni.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+ <link rel="stylesheet" type="text/css" href="dragonsStyle.css">
+</head>
+<body>
+<h1>This is the Alumni page<h1>
+</body>
+</html>
--- web/dragonsJS.js
+++ web/dragonsJS.js
@@ -54,18 +54,8 @@ function loadAlumni() {
      document.getElementById("content").innerHTML = xhttp.responseText;
     }
   };
-  xhttp.open("GET", "https://morning-basin-25235.herokuapp.com/dragonsAlumni.html", true);
+  xhttp.open("GET", "https://morning-basin-25235.herokuapp.com/dragonsAlumni.php", true);
   xhttp.send();
 }
 
-//Load Information
-function loadInfo() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (xhttp.readyState == 4 && xhttp.status == 200) {
-     document.getElementById("content").innerHTML = xhttp.responseText;
-    }
-  };
-  xhttp.open("GET", "https://morning-basin-25235.herokuapp.com/dragonsInfo.html", true);
-  xhttp.send();
 }
