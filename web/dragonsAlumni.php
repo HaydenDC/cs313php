3b13b958d2a3a9dcfbba87b79c1a92434e9f2a61
Hayden <car12039@byui.edu>
2016-10-15 13:42:58 -0600

    added header tag

--- web/dragonsAlumni.php
+++ web/dragonsAlumni.php
@@ -3,6 +3,8 @@
  <link rel="stylesheet" type="text/css" href="dragonsStyle.css">
 </head>
 <body>
-<h1>This is the Alumni page<h1>
+ <header>
+  <h1>This is the Alumni page</h1>
+ </header>
 </body>
 </html>
