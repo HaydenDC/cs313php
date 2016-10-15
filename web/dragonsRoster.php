b856ad2ebfc1094787309a0ec4b73e76bf16b247
Hayden <car12039@byui.edu>
2016-10-15 15:11:20 -0600

    created php

--- web/dragonsRoster.php
+++ web/dragonsRoster.php
@@ -6,5 +6,44 @@
  <header>
   <h1>This is the Roster page</h1>
  </header>
+ <?php
+
+// default Heroku Postgres configuration URL
+$dbUrl = getenv('DATABASE_URL');
+
+if (empty($dbUrl)) {
+ // example localhost configuration URL with postgres username and a database called cs313db
+ $dbUrl = "postgres://postgres:password@localhost:5432/cs313db";
+}
+
+$dbopts = parse_url($dbUrl);
+
+
+$dbHost = $dbopts["host"]; 
+$dbPort = $dbopts["port"]; 
+$dbUser = $dbopts["user"]; 
+$dbPassword = $dbopts["pass"];
+$dbName = ltrim($dbopts["path"],'/');
+
+try {
+ $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+
+	foreach($db->query('SELECT * FROM player')as $row)
+	{
+		echo '<p>';
+		echo '<strong>' . $row['name'] . '' . $row['pNumber'] . '';
+		echo $row['position'] . '' . $row['yrsPlayed'];
+		echo $row['homeTown'] . ', ' . $row['homeState'];
+		echo $row['homeCountry'] . '' . $row['major'] . '.';
+		echo '</p>';
+	}
+}
+catch (PDOException $ex) {
+ print "<p>error: $ex->getMessage() </p>\n\n";
+ die();
+}
+
+?>
 </body>
 </html>
