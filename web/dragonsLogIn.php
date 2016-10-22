77dc5292ec76d94a837e3ee6504b82be78667c4d
Hayden <car12039@byui.edu>
2016-10-22 02:32:55 -0600

    player form

--- web/dragonsLogIn.php
+++ web/dragonsLogIn.php
@@ -10,6 +10,83 @@
    <h1> Add a player</h1>
   </header>
 <div id="innerContent">
+
+<form>
+You are:
+<input type="radio" name="dStatus" value="player">A Player<br/>
+<input type="radio" name="dStatus" value="Alumni">An Alumni<br/>
+Name:<br/>
+<input type="text" name="name" placeholder="ex. John Doe"><br/>
+Jersey Number:
+<input type="text" name="jNum" placeholder="ex. 17"><br/>
+Position:
+<input type="text" name="Pos" placeholder="ex. Outside Defender"><br/>
+Years Played:
+<input type="text" name="yrsPlayed" placeholder="ex. 3"><br/>
+Home Town:
+<input type="text" name="hTown" placeholder="ex. Seattle"><br/>
+Home State:
+<input list="states" name="hState">
+ <datalist id="states">
+  <option value="AL">
+  <option value="AK">
+  <option value="AZ">
+  <option value="AR">
+  <option value="CA">
+  <option value="CO">
+  <option value="CT">
+  <option value="DE">
+  <option value="FL">
+  <option value="GA">
+  <option value="HI">
+  <option value="ID">
+  <option value="IL">
+  <option value="IN">
+  <option value="IA">
+  <option value="KS">
+  <option value="KY">
+  <option value="LA">
+  <option value="ME">
+  <option value="MD">
+  <option value="MA">
+  <option value="MI">
+  <option value="MN">
+  <option value="MS">
+  <option value="MO">
+  <option value="MT">
+  <option value="NE">
+  <option value="NV">
+  <option value="NH">
+  <option value="NJ">
+  <option value="NM">
+  <option value="NY">
+  <option value="NC">
+  <option value="ND">
+  <option value="OH">
+  <option value="OK">
+  <option value="OR">
+  <option value="PA">
+  <option value="RI">
+  <option value="SC">
+  <option value="SD">
+  <option value="TN">
+  <option value="TX">
+  <option value="UT">
+  <option value="VT">
+  <option value="VA">
+  <option value="WA">
+  <option value="WV">
+  <option value="WI">
+  <option value="WY">
+ </datalist> 
+  Home Country:
+  <input type="text" name="hCountry" placeholder="ex. Brazil"><br/>
+  Major:
+  <input type="text" name="major" placeholder="ex. Business"><br/>
+
+  <input type="submit">Submit
+
+</form>
  <?php
 
 //Database URL
