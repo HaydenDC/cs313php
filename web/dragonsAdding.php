ff72051c3535bd6ebeb490aa41d082bb16ec5b67
Hayden <car12039@byui.edu>
2016-10-22 21:47:15 -0600

    changed to ..Adding

--- web/dragonsAdding.php
+++ web/dragonsAdding.php
@@ -16,19 +16,19 @@
 //Database URL
 $dbUrl = getenv('DATABASE_URL');
 
-if (empty($dbUrl)) {
+//if (empty($dbUrl)) {
  //Connect to the Database
- $dbUrl = "postgres://postgres:password@localhost:5432/dragonsdb";
-}
+ //$dbUrl = "postgres://postgres:password@localhost:5432/dragonsdb";
+//}
 
-$dbopts = parse_url($dbUrl);
+//$dbopts = parse_url($dbUrl);
 
 
-$dbHost = $dbopts["host"]; 
-$dbPort = $dbopts["port"]; 
-$dbUser = $dbopts["user"]; 
-$dbPassword = $dbopts["pass"];
-$dbName = ltrim($dbopts["path"],'/');
+//$dbHost = $dbopts["host"]; 
+//$dbPort = $dbopts["port"]; 
+//$dbUser = $dbopts["user"]; 
+//$dbPassword = $dbopts["pass"];
+//$dbName = ltrim($dbopts["path"],'/');
 
 //try {
     //$conn = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
