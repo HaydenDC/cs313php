c00826d0230f74d76e640050fbef816fce5d9d4f
Hayden <car12039@byui.edu>
2016-10-22 21:35:41 -0600

    commented for debugging

--- web/dragonsLogIn.php
+++ web/dragonsLogIn.php
@@ -12,24 +12,13 @@
 <a href="https://morning-basin-25235.herokuapp.com/dragonsHome.html">Back to home page</a>
 
  <?php
-$name = htmlspecialchars($_POST["name"]);
-//$jNum = htmlspecialchars($_POST["jNum"]);
-//$Pos = htmlspecialchars($_POST["Pos"]);
-//$yrsPlayed = htmlspecialchars($_POST["yrsPlayed"]);
-//$hTown = htmlspecialchars($_POST["hTown"]);
-//$hState = htmlspecialchars($_POST["hState"]);
-//$hCountry = htmlspecialchars($_POST["hCountry"]);
-//$major = htmlspecialchars($_POST["major"]);
-
-//	echo $name . $jNum . $Pos . $yrsPlayed . $hTown . $hState . $hCountry . $major "all came from POST";
-
 //Database URL
-//$dbUrl = getenv('DATABASE_URL');
+$dbUrl = getenv('DATABASE_URL');
 
-//if (empty($dbUrl)) {
+if (empty($dbUrl)) {
  //Connect to the Database
- //$dbUrl = "postgres://postgres:password@localhost:5432/dragonsdb";
-//}
+$dbUrl = "postgres://postgres:password@localhost:5432/dragonsdb";
+}
 
 //$dbopts = parse_url($dbUrl);
 
