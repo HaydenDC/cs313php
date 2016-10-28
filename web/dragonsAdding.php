396921b03a51394de12896f1ff7c0f296bae169f
Hayden <car12039@byui.edu>
2016-10-27 22:01:38 -0600

    fixing Adding.php

--- web/dragonsAdding.php
+++ web/dragonsAdding.php
@@ -61,7 +61,7 @@ echo "Making PDO";
  
  
     //Set the PDO error mode to exception
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   // $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 echo "Made 'setAttribute'";
 /*
 
