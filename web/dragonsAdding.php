d217fb3ef76c88a763113398fa5f7d24bba46548
Hayden <car12039@byui.edu>
2016-10-27 21:45:54 -0600

    fixing Adding.php

--- web/dragonsAdding.php
+++ web/dragonsAdding.php
@@ -59,14 +59,14 @@ echo "Making PDO";
  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
  echo "Made the PDO";
  
-
+/*
 try
 {
     //Set the PDO error mode to exception
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 echo "Made 'setAttribute'";
 
-/*
+
     $sql = "INSERT INTO player (name, pNum, position, yrsplayed, hometown, homestate, homecountry, major)
     VALUES ('$name', '$jNum', '$Pos', '$yrsPlayed', '$hTown', '$hState', '$hCountry', '$major')";
 	
@@ -74,7 +74,7 @@ echo "Made 'setAttribute'";
     exec($db, $sql);
     $newId = $db->lastInsertId();
     echo "New player added";
-*/
+
     }
 
 
